1d9d9e68efd9a9d79906d8618aa946dcd4fa76b3
Allan Hall <bigal76126@gmail.com>
2019-05-16 16:08:11 -0400

    added api image

--- public/main.js
+++ public/main.js
@@ -1,7 +1,13 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(api => {
+      console.log(api.url)
+      document.querySelector('.explore').style.backgroundImage = `url(${
+        api.url
+      })`
+    })
 }
-
 document.addEventListener('DOMContentLoaded', main)
